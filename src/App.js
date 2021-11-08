79b8e6c6bbbc2e02f9035e116841c82eb80d02db
nikhil andola <nikhilandola123@gmail.com>
2021-11-08 09:12:18 +0530

    "Pokemon stats using react and API"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Axios from "axios";
 
 function App() {
+  const [pokemonName, setPokemonName] = useState("");
+  const [pokemonChosen, setPokemonChosen] = useState(false);
+  const [pokemon, setPokemon] = useState({ 
+    name: "",
+    species: "",
+    img: "",
+    hp: "",
+    attack: "",
+    defence: "",
+    type: "",
+  });
+
+  const searchPokemon = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName} `).then(
+      (response) => {
+        console.log(response);
+        setPokemon({
+          name: pokemonName,
+          species: response.data.species.name,
+          img: response.data.sprites.front_default,
+          hp: response.data.stats[0].base_stat,
+          attack: response.data.stats[1].base_stat,
+          defence: response.data.stats[2].base_stat,
+          type: response.data.types[0].type.name,
+        });
+        setPokemonChosen(true);
+      }
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="TitleSection">
+        <h1>Pokemon Stats</h1>
+        <input
+          type="text"
+          onChange={(event) => {
+            setPokemonName(event.target.value);
+          }}
+        />
+
+        <button onClick={searchPokemon}>Search Pokemon</button>
+      </div>
+          <div className="displaySection">
+            {!pokemonChosen ? (<h1>Please chose a pokemon </h1>) : 
+          (
+          <>
+            <h1>{pokemon.name}</h1> 
+            <img src={pokemon.img} alt="pokemon_image"/>
+            <h3>Species: {pokemon.species}</h3>
+            <h3>Type: {pokemon.type}</h3>
+            <h4>Hp: {pokemon.hp}</h4>
+            <h4>Attack: {pokemon.attack}</h4>
+            <h4>Defence: {pokemon.defence}</h4>
+          </>
+          )}
+          </div>
     </div>
   );
 }
---
+++ src/components/useEffectApi.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const UseEffectApi = () => {
+    return (
+        <>
+            <h1>List of github users</h1>
+            <div className="container-fluid mt-5">
+                <div className="row text-center">
+                    <div className="col-10 col-md-4 mt-5">
+                        <div class="card p-2">
+                            <div class ="d-flex align-items-center">
+                                <div className="image"> <img src="" class="rounded" width="155"/></div>
+                                <div className="ml-3 w-100">
+                                    
+                            <h4 class ="card-title">Card title</h4>
+                                    </div>
+                            <p class ="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class ="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default UseEffectApi
